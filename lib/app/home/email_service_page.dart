b8386401473fb9b9167324b1eac97268591f799f
Hussein <husseinelghoul@yahoo.com>
2020-04-27 01:12:05 +0300

    changed Email Service page

--- lib/app/home/email_service_page.dart
+++ lib/app/home/email_service_page.dart
@@ -1,39 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intro_slider/intro_slider.dart';
+class EmailServicePage extends StatefulWidget {
+  @override
+  _EmailServicePage createState() => _EmailServicePage();
+}
 
-class EmailServicePage extends StatelessWidget {
+class _EmailServicePage extends State<EmailServicePage> {
+  List<Slide> slides = new List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      new Slide(
+        title: "Email Service",
+        description: "You can  create a Session without using your phone thanks to our Email Service !",
+        styleDescription:
+            TextStyle(color: Colors.white, fontSize: 20.0),
+        pathImage: "assets/email.png",
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "Email Service",
+        description: """You just put the session information in a txt file using the following format \n\ntitle: Title
+        options: option1,option2,option3""",
+                styleDescription:
+            TextStyle(color: Colors.white, fontSize: 20.0),
+        pathImage: "assets/txt.png",
+        backgroundColor: Colors.deepPurple[200],
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "Email Service",
+        description:
+            "Finaly send it as attachement to \n\nsfueokfb@mailparser.io\n\n and set the email subject to the room token where you wish to create the sesion",
+             styleDescription:
+            TextStyle(color: Colors.white, fontSize: 20.0),
+        pathImage: "assets/quiz.png",
+        backgroundColor: Color(0xff9932CC),        
+      ),
+    );
+  }
+
+  void onDonePress() {
+    Navigator.pop(context);
+  }
+
+  void onSkipPress() {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(), 
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text('Email Service', 
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              )
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-"""In order to do so you can send an email message to the following address:\n
-sfueokfb@mailparser.io\n
-with your room token as you can now create a session using any txt file without using your phone,\n
-you just put the session information in a txt file using the following format:\n
-title: Title
-options: option1, option2, option3\n
-and email it to with your room token only as email subject """,
-              style: TextStyle(
-                fontSize: 20
-              )),
-            )
-          ],
-        ),
-      )
+    return new IntroSlider(
+      slides: this.slides,
+      onDonePress: this.onDonePress,
+      onSkipPress: this.onSkipPress,
     );
   }
 }
